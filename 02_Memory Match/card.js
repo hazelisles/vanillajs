c730adfb6ca0c53fd27d3dbc2da8c112a2cd4686
hazelisles <zhccj19@gmail.com>
2022-02-25 23:10:08 -0800

    complete shuffling function, display whole deck

--- 02_Memory Match/card.js
+++ 02_Memory Match/card.js
@@ -1,6 +1,6 @@
 const utility = {
   newDeck() {
-    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+    const numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
     const symbols = ["/02_Memory Match/Clubs.png", "/02_Memory Match/Diamonds.png", "/02_Memory Match/Hearts.png", "/02_Memory Match/Spades.png"];
     const deck = [];
     for (let i = 0; i < symbols.length; i++) {
@@ -9,12 +9,21 @@ const utility = {
       }
     }
     return deck;
+  },
+  getRandomOrderArray(num) {
+    // use Fisher-Yates Shuffle to thorough shuffling
+    const array = Array.from(Array(num).keys());
+    for (let i = array.length - 1; i > 0; i--) {
+      let randIndex = Math.floor(Math.random() * (i + 1));
+      [array[i], array[randIndex]] = [array[randIndex], array[i]];
+    }
+    return array;
   }
 }
 const view = {
   displayCards() {
     const board = document.getElementById("cards");
-    board.innerHTML = this.getCardElement(40);
+    board.innerHTML = utility.getRandomOrderArray(52).map(i => this.getCardElement(i)).join("");
   },
   getCardElement(index) {
     const deck = utility.newDeck();
